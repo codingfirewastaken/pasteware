20de1e91173e0eda531c33e1380a36c6313f32da
codingfire <88985009+codingfirewastaken@users.noreply.github.com>
2025-03-30 16:04:08 -0400

    Update Tenacity.java

--- src/main/java/dev/tenacity/Tenacity.java
+++ src/main/java/dev/tenacity/Tenacity.java
@@ -35,8 +35,8 @@ public class Tenacity implements Utils {
 
     public static final Tenacity INSTANCE = new Tenacity();
 
-    public static final String NAME = "Tenacity";
-    public static final String VERSION = "6.0";
+    public static final String NAME = "pasteware";
+    public static final String VERSION = "1.0";
     public static final ReleaseType RELEASE = ReleaseType.DEV;
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final File DIRECTORY = new File(mc.mcDataDir, NAME);
